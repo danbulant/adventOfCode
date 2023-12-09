6babebd78199190a808dcc7e91c5ce2e1a9c5d47
Daniel Bulant <danbulant@gmail.com>
2023-12-09 15:47:33 +0100

    day 9

---
+++ 2023/9/part1.py
@@ -0,0 +1,22 @@
+def get_differences(numbers: list[int]) -> list[int]:
+    differences = []
+    for i in range(1, len(numbers)):
+        differences.append(numbers[i] - numbers[i - 1])
+    return differences
+
+values = []
+
+for line in open("./input2"):
+    history = [[int(x) for x in line.split(" ")]]
+    while not all(x == 0 for x in history[-1]):
+        history.append(get_differences(history[-1]))
+    history.reverse()
+    for i in range(len(history)):
+        if i == 0:
+            history[i].append(0)
+            continue
+        history[i].append(history[i][-1] + history[i - 1][-1])
+    values.append(history[-1][-1])
+
+print(values)
+print(sum(values))
